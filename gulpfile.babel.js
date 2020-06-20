e8edb8482fa13bd6b87781d530f3c368acc0c8d8
dovigod <inaki96@naver.com>
2020-06-20 20:50:59 +0900

    add watcher , now auto reloading

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,8 +7,8 @@ import WS from "gulp-webserver";
 
 const routes = {
     pug:{
-        src: "src/*.pug",
-
+        src: "src/*.pug", //애를 감시하면 제대로 작동안함 고로 모든 파일을 감시해야해
+        watch: "src/**/*.pug",
         dest:"goal"
     },
     scss:{
@@ -29,9 +29,17 @@ const webserver = () => gulp.src("goal/").pipe(WS({
     livereload: true,
     open: true
 }))
+
+const watchTarget = () =>{
+    gulp.watch(routes.pug.watch,pug);
+}
 //내가 커맨드 할것만 export 하면댐
 
 const prepare = gulp.series([clear]);
-const serverActive = gulp.series([webserver]);
+const serverActive = gulp.parallel([webserver,watchTarget]); //동시 실행
 const assets =  gulp.series([pug]);
+
 export const dev = gulp.series([prepare,assets,serverActive]);
+
+
+///문제, 이제 여기선 reload기능이 안먹힘 --> gulp가 내 pug파일을 감시안하기 때문
